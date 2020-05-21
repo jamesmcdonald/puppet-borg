efec54809b4d43982a8032ae52233d2c97dc824d
James McDonald <james@jamesmcdonald.com>
2020-05-21 14:07:16 +0200

    Fix Profile reference in collector

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,6 +1,6 @@
 # Set up stuff on the borg server
 class borg::server {
-  Profile::Borg::Client <<| tag=='borg-derpy' |>> {
+  Borg::Client <<| tag=='borg-derpy' |>> {
     borg_base => '/tank/backup/borg',
     borg_user => 'borg',
   }
