c5bd547300f78d4f3374db104c335d4ee35f2f4d
James McDonald <james@jamesmcdonald.com>
2020-05-21 15:07:56 +0200

    Add empty defaults and failure check to exported borg::client

--- manifests/client.pp
+++ manifests/client.pp
@@ -2,9 +2,13 @@
 define borg::client (
   $fqdn,
   $key,
-  $borg_base,
-  $borg_user,
+  $borg_base = '',
+  $borg_user = '',
 ){
+  if $borg_base == '' or $borg_user == '' {
+    fail('borg::client needs $borg_base and $borg_user defined')
+  }
+
   ssh_authorized_key{$name:
       options => ["command=\"cd ${borg_base}; borg serve --restrict-to-repository ${fqdn}\"", 'restrict'],
       user    => $borg_user,
