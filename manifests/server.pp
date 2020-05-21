2075623f5292ab0c030746f8cf0865cbff667d76
James McDonald <james@jamesmcdonald.com>
2020-05-21 16:25:20 +0200

    Add borg server setup

--- manifests/server.pp
+++ manifests/server.pp
@@ -3,7 +3,22 @@ class borg::server (
   String $user = 'borg',
   String $base_dir = '/var/lib/borg',
   String $export_tag = 'borg',
+  String $borgpackage = 'borgbackup',
 ){
+  ensure_packages($borgpackage)
+
+  file {$base_dir:
+    ensure => directory,
+    mode   => '0700',
+    owner  => $user,
+    group  => $user,
+  }
+
+  user {$user:
+    ensure     => present,
+    home       => $base_dir,
+  }
+
   Borg::Client <<| tag==$export_tag |>> {
     borg_base => $base_dir,
     borg_user => $user,
