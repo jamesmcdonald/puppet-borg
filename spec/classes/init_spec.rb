ffd4772183bfcc9006a4f0284a1eb0968839aabd
James McDonald <james@jamesmcdonald.com>
2020-05-26 13:32:42 +0200

    Fix lint errors

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -5,14 +5,16 @@ require 'spec_helper'
 describe 'borg' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      let(:params) { {
-        :passphrase => "secrets",
-        :server_address => "server.example.com",
-      } }
+      let(:params) do
+        {
+          passphrase: 'secrets',
+          server_address: 'server.example.com',
+        }
+      end
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
-      it { should contain_class('borg') }
+      it { is_expected.to contain_class('borg') }
     end
   end
 end
